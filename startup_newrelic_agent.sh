b54eb46bc2c70070e46338b8a130a9c068b06464
Philip Sahli <philip@sahli.net>
2015-10-01 07:01:49 +0200

    cat agent configfile

--- startup_newrelic_agent.sh
+++ startup_newrelic_agent.sh
@@ -3,5 +3,8 @@
 # newrelic plugin
 if [ ! -z "$NEWRELIC_LICENSE" ]; then
         j2 /etc/newrelic/newrelic-plugin-agent.cfg | sponge /etc/newrelic/newrelic-plugin-agent.cfg
+	echo "******** File newrelic-plugin-agent.cfg rendered START ***********"
+        cat /etc/newrelic/newrelic-plugin-agent.cfg
+	echo "******** File newrelic-plugin-agent.cfg rendered END ***********"
         sed -i s/autostart=false/autostart=true/ /etc/supervisor/conf.d/newrelic-plugin.conf
 fi
